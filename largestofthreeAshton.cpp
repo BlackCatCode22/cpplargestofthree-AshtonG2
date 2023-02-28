c0e9deb42ad48d1c2a5e14bfb6dcf17402875713
AshtonG2 <ashtong9000@gmail.com>
2023-02-28 15:07:52 -0800

    My Largest of Three

--- largestofthreeAshton.cpp
+++ largestofthreeAshton.cpp
@@ -1,47 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "\n Largest of Three\n";
+//Reference: https://www.programiz.com/cpp-programming/examples/largest-number-among-three
+
+int main() {
+    
+    double n1, n2, n3;
+
+    cout << "Pick 3 Numbers: ";
+    cin >> n1 >> n2 >> n3;
+
+    if(n1 >= n2 && n1 >= n3)
+        cout << "Largest: " << n1;
+
  
-    int num1 = 0;
-    int num2 = 0;
-    int num3 = 0;
-    int largest = 0;
-    int total = 0;
-
-    cout << "\n First Number: ";
-    std::cin >> num1;
-
-    cout << "\n Second Number: ";
-    std::cin >> num2;
-
-    cout << "\n Third Number: ";
-    std::cin >> num3;
-
-    total = num1 + num2 + num3;
-
-    if (num1 > num2)
-    {
-        if (num1 > num3)
-        { largest = num1;
-        }
-        else
-        { largest = num3;
-        }
-    }
-    else
-    {
-        if (num2 > num3)
-        { largest = num2;
-        }
-        else 
-        { largest = num3;
-        }
-    }
-   
-    std::cout << "\n\n The largest of " << num1 << " and " << num2 << " and " << num3 << " is " << largest;
-    std::cout << "\n\n The total is " << total << "\n\n";
-    std::cout << "\n\n The largest is " << largest << "\n\n";
-
+    else if(n2 >= n1 && n2 >= n3)
+        cout << "Largest: " << n2;
+    
+    else 
+        cout << "Largest: " << n3;
+  
+    return 0;
 }
